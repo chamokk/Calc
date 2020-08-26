9c503966dbb18db4b42f76ca1b5dc1bde5e0b488
Pedro Suarez <pedro20q@gmail.com>
2020-08-26 19:06:52 -0400

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Container} from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 
 
 class App extends Component {
