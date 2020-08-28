7b7a495c9cdc32123147e75cf7e9cf7166514e86
Pedro Suarez <pedro20q@gmail.com>
2020-08-27 20:24:39 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,44 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Grid, Segment, Container, GridRow} from 'semantic-ui-react';
+import { Button, Grid, Segment, Container, GridRow } from 'semantic-ui-react';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      operator: "",
+      input: "",
+      currentVal: "",
+      previusVal: ""
+    }
+  }
+
+  addToInput = (val) => {
+    this.setState({input: this.state.input + val});
+  };
+
+  addOperator = (val) => {
+    this.setState({previusVal: this.state.input});
+    this.setState({operator: val});
+    this.setState({input: ""});
+  };
+
+  equalOp = () => {
+    this.setState({currentVal: this.state.input});
+
+    
+  }
+
 
 render() {
   return (
-    <Container className='container' > 
+    <Container className='container'> 
       <Grid centered>
         <Segment>
-          <GridRow fluid color='black'>
-            <Segment size='big' color='black' inverted textAlign='right'>1</Segment>
+          <GridRow>
+            <Segment size='big' color='black' inverted textAlign='right'
+            >{this.state.input}</Segment>
           </GridRow>
           <Grid.Row centered>
-             <Button size='big'>7</Button>
-             <Button size='big'>8</Button>
-             <Button size='big'>9</Button>
-             <Button size='big'>/</Button>
+             <Button size='big' onClick={()=>this.addToInput(7)}>7</Button>
+             <Button size='big' onClick={()=>this.addToInput(8)}>8</Button>
+             <Button size='big' onClick={()=>this.addToInput(9)}>9</Button>
+             <Button size='big' onClick={()=>this.addOperator('/')}>/</Button>
           </Grid.Row>
           <Grid.Row centered>
-             <Button size='big'>4</Button>
-             <Button size='big'>5</Button>
-             <Button size='big'>6</Button>
-             <Button size='big'>*</Button>
+             <Button size='big' onClick={()=>this.addToInput(4)}>4</Button>
+             <Button size='big' onClick={()=>this.addToInput(5)}>5</Button>
+             <Button size='big' onClick={()=>this.addToInput(6)}>6</Button>
+             <Button size='big' onClick={()=>this.addOperator('*')}>*</Button>
           </Grid.Row>
           <Grid.Row centered>
-             <Button size='big'>1</Button>
-             <Button size='big'>2</Button>
-             <Button size='big'>3</Button>
-             <Button size='big'>+</Button>
+             <Button size='big' onClick={()=>this.addToInput(1)}>1</Button>
+             <Button size='big' onClick={()=>this.addToInput(2)}>2</Button>
+             <Button size='big' onClick={()=>this.addToInput(3)}>3</Button>
+             <Button size='big' onClick={()=>this.addOperator('+')}>+</Button>
           </Grid.Row>
           <Grid.Row centered>
-             <Button size='big'>.</Button>
-             <Button size='big'>0</Button>
+             <Button size='big' onClick={()=>this.addToInput('.')}>.</Button>
+             <Button size='big' onClick={()=>this.addToInput(0)}>0</Button>
              <Button size='big'>=</Button>
-             <Button size='big'>-</Button>
+             <Button size='big'onClick={()=>this.addOperator('-')}>-</Button>
           </Grid.Row>
-          <GridRow><Button fluid size='big'>Clear</Button></GridRow>
+          <GridRow>
+            <Button fluid size='big'onClick={()=>this.setState({input: ""})}>Clear</Button>
+            </GridRow>
         </Segment>
       </Grid>
     
